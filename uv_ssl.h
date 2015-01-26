e74dccce0ba35450c6f422efcc49d573f9665092
deleisha <dlmeetei@gmail.com>
2015-01-27 01:22:53 +0530

    Added licence to C file and a basic python utility for license prepend is worked on

--- uv_ssl.h
+++ uv_ssl.h
@@ -1,3 +1,26 @@
+/**********************************************************************************
+ *   The MIT License (MIT)
+
+ *   Copyright (c) 2015  deleisha <dlmeetei@gmail.com>
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+***********************************************************************************/
 #ifndef __UV_SSL_H__
 #define __UV_SSL_H__
 
@@ -345,7 +368,6 @@ void on_tcp_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
         uv_ssl_t *s_ssl = (uv_ssl_t*) client->data;
         int rv = BIO_write( s_ssl->app_bio_, buf->base, nread);
         uv__ssl_err_hdlr(s_ssl, client, rv);
-        fprintf(stderr, "BIO_write = %d\n", rv);
 
         uv__ssl_read(s_ssl, client, (uv_buf_t*)buf, nread);
     }
