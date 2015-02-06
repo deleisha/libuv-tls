e8d019a05f23d281e77421fe0e7efcbb6974339f
deleisha <dlmeetei@gmail.com>
2015-02-06 11:22:33 +0530

    Initial non working code for client part

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
+all: echo tls_client
 echo: test_tls.c
 	make -C ./libuv/out
 	clang -g -Wall -o echo test_tls.c uv_tls.c libuv/out/Debug/libuv.a -lpthread -lssl -lcrypto
 
+tls_client: test_tls_client.c
+	make -C ./libuv/out
+	clang -g -Wall -o tls_client test_tls_client.c uv_tls.c libuv/out/Debug/libuv.a -lpthread -lssl -lcrypto
+
 clean:
 	-rm echo
+	-rm tls_client
---
+++ test_tls_client.c
@@ -0,0 +1,110 @@
+/*//////////////////////////////////////////////////////////////////////////////
+
+ * Copyright (c) 2015  deleisha and other libuv-tls contributors
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+**////////////////////////////////////////////////////////////////////////////*/
+
+
+#include "uv_tls.h"
+
+void on_write(uv_write_t *req, int status)
+{
+    if(!status && req) {
+        free(req);
+        req = 0;
+    }
+}
+
+void on_close(uv_tls_t* h)
+{
+    free(h);
+    h = 0;
+}
+
+
+//Callback for testing
+void on_read(uv_tls_t* clnt, int nread, uv_buf_t* dcrypted)
+{
+    if( nread <= 0 ) {
+        if( nread == UV_EOF) {
+            uv_tls_close(clnt, on_close);
+        }
+        return;
+    }
+
+    uv_write_t *rq = (uv_write_t*)malloc(sizeof(*rq));
+    assert(rq != 0);
+    uv_tls_write(rq, clnt, dcrypted, on_write);
+}
+
+void alloc_cb(uv_handle_t *handle, size_t size, uv_buf_t *buf)
+{
+    buf->base = (char*)malloc(size);
+    memset(buf->base, 0, size);
+    buf->len = size;
+    assert(buf->base != NULL && "Memory allocation failed");
+}
+
+
+
+//TEST CODE for the lib
+void on_connect(uv_connect_t *req, int status)
+{
+    if( status ) {
+        fprintf( stderr, "TCP connection error\n");
+        return;
+    }
+    fprintf( stderr, "TCP connection established\n");
+
+    uv_write_t *rq = (uv_write_t*)malloc(sizeof(*rq));
+    assert(rq != 0);
+    //uv_tls_write(rq, clnt, dcrypted, on_write);
+}
+
+
+int main()
+{
+    uv_loop_t *loop = uv_default_loop();
+
+    uv_tls_t *client = (uv_tls_t*)malloc(sizeof *client);
+    if(uv_tls_init(loop, client) < 0) {
+        free(client);
+        client = 0;
+        fprintf( stderr, "TLS setup error\n");
+        return  -1;
+    }
+
+    const int port = 8000;
+    struct sockaddr_in conn_addr;
+    int r = uv_ip4_addr("127.0.0.1", port, &conn_addr);
+    assert(!r);
+
+    uv_connect_t req;
+    uv_tls_connect(&req, client, (const struct sockaddr*)&conn_addr, on_connect);
+
+    uv_run(loop, UV_RUN_DEFAULT);
+
+
+    uv_tls_shutdown(client);
+    free (client);
+    client = 0;
+
+    return 0;
+}
